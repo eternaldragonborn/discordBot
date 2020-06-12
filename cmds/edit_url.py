41c2c91e23dde4a40fc5bcc0992ae1ebd53c00e0
eternaldragonbo <k0963671380@gmail.com>
2020-06-12 07:25:33 +0000

    url format change

--- cmds/edit_url.py
+++ cmds/edit_url.py
@@ -8,19 +8,20 @@ authorId = core.wrFiles.readFile("setting")["authorId"]
 
 class EDIT_URL(Cog_Ext):
 	@commands.command()
-	async def add_url(self, cxt, item, url):
+	async def add_url(self, cxt, item, url, weight :int=1):
 		if cxt.author.id == authorId:
 			if item in urls.keys():
 				try:
-					urls[item].append(url)
+					newurl = {url : weight}
+					urls[item].update(newurl)
 				except:
 					await cxt.send("新增失敗", delete_after = 3)
 				else:
 					await cxt.send("新增完畢", delete_after = 3)
 			else:
 				try:
-					urls[item] = []
-					urls[item].append(url)
+					newitem = {item : {url : weight}}
+					urls.update(newitem)
 				except:
 					await cxt.send("新增失敗", delete_after = 3)
 				else:
@@ -41,13 +42,26 @@ class EDIT_URL(Cog_Ext):
 					core.wrFiles.writeFile("others", urls)
 			else:
 				try:
-					urls[item].remove(url)
+					del urls[item][url]
 				except:
 					await cxt.send("刪除失敗", delete_after = 3)
 				else:
 					await cxt.send("刪除成功", delete_after = 3)
 					core.wrFiles.writeFile("others", urls)
 		await cxt.message.delete()
+
+	@commands.command()
+	async def edit_weight(self, ctx, item, url, weight :int):
+		if ctx.author.id == authorId:
+			try:
+				urls[item][url] = weight
+			except Exception as e:
+				await ctx.send("更改失敗", delete_after = 3)
+				print(e,"\n")
+			else:
+				await ctx.send("更改成功", delete_after = 3)
+				core.wrFiles.writeFile("others", urls)
+		await ctx.message.delete()
 	
 
 
--- events/event.py
+++ events/event.py
@@ -5,26 +5,25 @@ import random
 from core.wrFiles import readFile
 import re
 
-url = readFile("others")
-weights = url["weights"]
+
+def choice_url(item):
+	url = readFile("others")
+	return str(random.choices(list(url[item].keys()), weights = list(url[item].values()))).strip("[]'")
 
 class Events(Cog_Ext):
 	@commands.Cog.listener()
 	async def on_message(self, msg):
 		if msg.author != self.bot.user:
 			if re.search(r'[rR啊阿ㄚ]{4,}', msg.content):
-				await msg.channel.send(f"{msg.author.mention}\n{random.choice(url['rrr'])}",
-				    delete_after=4)
+				await msg.channel.send(f"{msg.author.mention}\n{choice_url('rrr')}", delete_after=4)
 			elif "loading cat" in msg.content.lower():
-				cat = str(random.choices(url["loadingCat"], weights = weights["loadingcat"])).strip("[]'")
-				await msg.channel.send(cat, delete_after=5)
+				await msg.channel.send(choice_url("loadingCat"), delete_after=5)
 			elif "星爆" in msg.content or "c8763" in msg.content.lower() or "starburst" in msg.content.lower():
 				await msg.add_reaction("\N{THUMBS DOWN SIGN}")
 			elif "虫合" in msg.content or "蛤" in msg.content:
-				await msg.channel.send(random.choice(url["what"]), delete_after=5)
+				await msg.channel.send(choice_url("what"), delete_after=5)
 			elif re.match(r'emmm', msg.content.lower()):
-				pic = str(random.choices(url["emmm"], weights = weights["emmm"])).strip("[]'")
-				await msg.channel.send(pic, delete_after = 5)
+				await msg.channel.send(choice_url("emmm"), delete_after = 5)
 
 		'''@commands.Cog.listener()
 		async def on_reaction_add(self, reaction, user):
@@ -32,8 +31,7 @@ class Events(Cog_Ext):
 
 	@commands.command()
 	async def loading(self, ctx, delay_time: int = 5):
-		cat = str(random.choices(url["loadingCat"], weights= weights["loadingcat"])).strip("[]'")
-		await ctx.send(cat, delete_after=delay_time)
+		await ctx.send(choice_url("loadingCat"), delete_after=delay_time)
 		await ctx.message.delete(delay=delay_time)
 
 
